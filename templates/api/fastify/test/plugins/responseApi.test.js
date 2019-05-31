f748d18d7d2a570be6aecaec0254aa0569359b3a
RafaelGSS <rafael.nunu@hotmail.com>
2019-05-31 09:55:10 -0300

    tests(responseApi): Implemented tests and added sget to package json

--- templates/api/fastify/test/plugins/responseApi.test.js
+++ templates/api/fastify/test/plugins/responseApi.test.js
@@ -1,18 +1,41 @@
 'use strict'
 
 const { test } = require('tap')
+const sget = require('simple-get').concat
+
 const Fastify = require('fastify')
+
 const responseApi = require('../../src/plugins/responseApi')
 
-test('responseApi works standalone', (t) => {
-  t.plan(2)
+test('responseApi decorateReply works in encapsulation', t => {
+  t.plan(7)
   const fastify = Fastify()
   fastify.register(responseApi)
 
-  fastify.ready((err) => {
+  fastify.register((instance, opts, next) => {
+    instance.get('/test-responseApi', (request, reply) => {
+      t.ok(reply.success, 'success decorator exists')
+      t.ok(reply.error, 'error decorator exists')
+      reply.send({ hello: 'world' })
+    })
+
+    next()
+  })
+
+  fastify.listen(0, err => {
     t.error(err)
-    t.equal(1, 1)
-    // t.equal(fastify.res.success().response.error, false)
+    fastify.server.unref()
+
+    sget({
+      method: 'GET',
+      url: 'http://localhost:' + fastify.server.address().port + '/test-responseApi'
+    }, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.strictEqual(response.headers['content-length'], '' + body.length)
+      t.deepEqual(JSON.parse(body), { hello: 'world' })
+      t.end()
+    })
     // t.equal(fastify.res.error().response.error, true)
   })
 })
