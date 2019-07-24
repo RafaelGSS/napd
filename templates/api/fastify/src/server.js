d06934a806c9331fdd16ee45f8b171fead7dc247
RafaelGSS <rafael.nunu@hotmail.com>
2019-07-24 10:46:15 -0300

    fix(api/fastify): Adjusted to bind network 0.0.0.0

--- templates/api/fastify/src/server.js
+++ templates/api/fastify/src/server.js
@@ -18,7 +18,7 @@ const app = Fastify({
 app.register(require('./app.js'))
 
 // Start listening.
-app.listen(process.env.PORT || 3000, (err) => {
+app.listen(process.env.PORT || 3000, '0.0.0.0', (err) => {
   if (err) {
     app.log.error(err)
     process.exit(1)
